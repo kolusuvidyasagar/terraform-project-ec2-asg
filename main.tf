7799a5de765a91c6365795e3996bec4bc4829097
kolusuvidyasagar <kolusu.vidyasagar@gmail.com>
2025-05-16 14:24:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,13 +3,14 @@ provider "aws" {
 }
 
 module "ec2_asg" {
-  source = "git::https://github.com/kolusuvidyasagar/terraform-project-ec2-asg-root.git//ec2-asg?ref=main"
+  source = "git::https://github.com/kolusuvidyasagar/terraform-project-ec2-asg-root.git?ref=main"
 
   project_name       = var.project_name
   ami_id             = var.ami_id
   instance_type      = var.instance_type
   key_name           = var.key_name
   vpc_id             = var.vpc_id
+  vpc_security_group_ids = [aws_security_group.asg_sg.id]
   subnet_ids         = var.subnet_ids
   min_size           = var.min_size
   max_size           = var.max_size
