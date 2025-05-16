62de30cc0e73933d0788dc0fa2546a6b6ef33f02
kolusuvidyasagar <kolusu.vidyasagar@gmail.com>
2025-05-16 15:20:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "ec2_asg" {
   instance_type      = var.instance_type
   key_name           = var.key_name
   vpc_id             = var.vpc_id
-  security_group_ids   = [aws_security_group.my_sg.id]
+  security_group_ids   = [aws_security_group.asg_sg.id]
   subnet_ids         = var.subnet_ids
   min_size           = var.min_size
   max_size           = var.max_size
