a14941e961037d5d397bfd50e95b59d6c10c7b2b
kolusuvidyasagar <kolusu.vidyasagar@gmail.com>
2025-05-16 22:26:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-provider "aws" {
-  region = var.region
+resource "aws_security_group" "asg_sg" {
+  # define your security group here
 }
 
 module "ec2_asg" {
@@ -10,7 +10,7 @@ module "ec2_asg" {
   instance_type      = var.instance_type
   key_name           = var.key_name
   vpc_id             = var.vpc_id
-  security_group_ids   = [aws_security_group.asg_sg.id]
+  security_group_ids = [aws_security_group.asg_sg.id]  # pass SG from root
   subnet_ids         = var.subnet_ids
   min_size           = var.min_size
   max_size           = var.max_size
